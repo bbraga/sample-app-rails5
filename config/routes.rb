abbbcbfbd7d6e4d4f65ad054fe28be210998a01e
Derrick Zhang <derrick.zhang@welab.co>
2016-02-27 15:54:47 +0800

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
 
+  get 'signup'  => 'users#new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
