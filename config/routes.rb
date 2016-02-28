cf4500c1dd42a2ab400a72a7ab586438f8ba73f5
Derrick Zhang <derrick.zhang@welab.co>
2016-02-28 08:57:24 +0800

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get 'signup'  => 'users#new'
 
+  resources :users, only: [:show, :create]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # Serve websocket cable requests in-process
